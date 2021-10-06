b03d6c7dafb2db0db35777ba04dccc4df9d5b4eb
Dmitrii Verbetsky <dmitrii@openocean.academy>
2021-10-06 23:23:42 +0300

    added webpack

---
+++ src/calc.js
@@ -0,0 +1,8 @@
+
+
+export default class Calc {
+
+    add(...args) {
+        return args.reduce((a, b) => a + b, 0);
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    mode: "development",
+
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg|jpeg|gif|ico)$/,
+                use: [
+                    { 
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'images',
+                            name: '[name-[sha1:hash:7].[ext]'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(ttf|otf|oet|woff|woff2)$/,
+                use: [
+                    { 
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'fonts',
+                            name: '[name].[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+}
